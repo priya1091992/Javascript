1413018e0a033107616f188cb6340bb718f951cf
Priya Choudhary <priya@tothenew.com>
2016-03-20 18:27:29 +0530

    add a new property in object

---
+++ add_new.property.js
@@ -0,0 +1,14 @@
+
+/**
+ * Created by priya on 20/3/16.
+ */
+
+var obj={
+  name:"Priya",
+  lastName:"Choudhary"
+
+}
+
+obj.age=24;
+obj.print=function(){return console.log(this.name, this.lastName, "age:",this.age);}
+obj.print();
