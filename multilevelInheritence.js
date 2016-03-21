2f6a6ce7b7afc3180ac2df8b81553ff765affe66
Priya Choudhary <priya@tothenew.com>
2016-03-21 16:57:13 +0530

    Multi level inheritence

--- multilevelInheritence.js
+++ multilevelInheritence.js
@@ -2,24 +2,23 @@
 // * Created by priya on 21/3/16.
 // */
 //
-function one(){
+function one1(){
   this.name="Priya"
 }
 
-function two(){
+function two1(){
 }
-two.prototype.age=24;
+two1.prototype.age=24;
 
-function three(){
+function three1(){
   this.gender="female";
 }
 
-one.prototype=two.prototype;
-var finalObj1= new one();
+one1.prototype=two1.prototype;
+var finalObj1= new one1();
+three1.prototype=finalObj1;
 
-three.prototype=finalObj1;
-
-var finalObj= new three();
+var finalObj= new three1();
 
 console.log(finalObj.name); //priya
 console.log(finalObj.age);  //24
@@ -35,12 +34,13 @@ function one(name){
 function two(age){
   this.age=age;
 }
-one.prototype=new two(24);
-
 function three(gender){
   this.gender=gender;
 }
+
+one.prototype=new two(24);
 three.prototype=new one("priya");
+
 var obj=new three("female");
 console.log("name:",obj.name);
 console.log("age:",obj.age);
