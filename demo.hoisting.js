84063252e53ab604a75836c99f6b37dc0c60f7ee
Priya Choudhary <priya@tothenew.com>
2016-03-22 10:56:14 +0530

    example of hoisting

---
+++ demo.hoisting.js
@@ -0,0 +1,15 @@
+/**
+ * Created by priya on 22/3/16.
+ */
+
+var name;
+
+//name="Priya";  if it is there typeof(name) is string
+
+function name(){
+  console.log("Priya");
+}
+
+name= "Priya"; //if it is not there typepf(name) is function
+
+console.log(typeof(name));
