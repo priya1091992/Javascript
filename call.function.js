5e11c16f51c3d618fd9639d49e4aebf017932381
Priya Choudhary <priya@tothenew.com>
2016-03-22 16:19:02 +0530

    Example of call

--- call.function.js
+++ call.function.js
@@ -4,12 +4,19 @@
 function Product(name, price) {
   this.name = name;
   this.price = price;
-  this.productPrice=20
+  this.productPrice=20;
+  console.log(">>>",this) // refers to food or toy
+  this.func=function(){
+    console.log("Hello");
+  }
 }
 
 function Food(name, price) {
-  Product.call(this, name, price);
+  console.log(this); //this refers to the Food
+  Product.call(this, name, price); // this here also refers to food
+  console.log(name)
   this.category = 'food';
+
 }
 
 function Toy(name, price) {
@@ -22,3 +29,4 @@ var fun = new Toy('robot', 40);
 
 console.log("In cheese object:",cheese.name , cheese.price, cheese.category, cheese.productPrice);
 console.log("In fun object:",fun.name , fun.price, fun.category, fun.productPrice);
+cheese.func();
