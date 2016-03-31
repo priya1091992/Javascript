4015bd0c0deef2f367c5cfea86626fcc7c323ee3
Priya Choudhary <priya@tothenew.com>
2016-03-31 17:39:12 +0530

    closure example

--- closure.demo1.js
+++ closure.demo1.js
@@ -6,32 +6,21 @@
 
 function one(value1, value2){
   var value3="value3";
-
   function two(){
     console.log(value1, value2, value3);
   }
   two();
-
 }
-
-one("value1", "value2"); //Output: value1 value2 value3
-
-
-
-
-
+one("value1", "value2"); //:::: value1 value2 value3
 
 
 //second example
-
 function outer(value1, value2){
   var value3="value3";
-
   function inner(){
     return value1+value2+value3;
   }
  return inner;
-
 }
 var func=outer("value1", "value2");
 
